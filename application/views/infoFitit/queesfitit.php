c428c85421ece25e09bebe5312c9b2d8f20fe11c
eric <eric.rodriguez.balsells@est.fib.upc.edu>
2018-01-31 20:55:31 +0100

    que es fitit actualizacion

--- application/views/infoFitit/queesfitit.php
+++ application/views/infoFitit/queesfitit.php
@@ -31,6 +31,62 @@
     width: 35px;
     height: auto;
   }
+  ul {
+    list-style-type: none;
+    list-style-position:inside;
+  }
+  ul img {
+    width: 35px;
+    height: auto;
+    margin-right: 20px;
+  }
+  ul .margin_top {
+    margin-top: 20px;
+  }
+  #fitit_font {
+      font-family: sans-serif;
+  }
+  .red {
+      color: #FF4548;
+  }
+  .grey {
+      color: #444444;
+  }
+  .fitit_style {
+      font-weight: bold;
+      font-style: italic;
+  }
+  button {
+    background: #FF4548;
+    color: white;
+    border-color: #FF4548;
+    text-align: center;
+  }
+  button:hover {
+    background: white;
+    color: #FF4548;
+    border-color: #FF4548;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  }
+  .number {
+    font-family: 'Asap', sans-serif;
+    background: #FF4548;
+    border-style: solid;
+    border-width: 5px;
+    border-color: #232323;
+    border-radius: 5px;
+    border-width: 2px;
+    font-size: 80px;
+    padding: 10px 25px;
+  }
+  .back {
+    background: #E9ECEF;
+    padding: 20px 40px;
+  }
+  .size {
+    font-size: 20px;
+  }
+
 </style>
 
 <img id="img-principal" src="http://localhost/fitit_v2/images/qef5.png">
@@ -55,8 +111,67 @@
 
   <div class="red">
       <h3 class="text-center text-uppercase margin_top bold">Somos la plataforma líder de asesoramientos fitness online en España</h3>
-      <h6 class="text-center margin_top">En FITIT te ponemos en contacto con tus coaches fitness favoritos para que puedas solicitarles dietas y entrenamientos de forma segura, profesional y transparente. Empezar una nueva vida enfocada al deporte y la alimentación saludable no es fácil, hazlo de la mano de los mejores profesionales.</h6>
+      <h6 class="text-center margin_top">En FITIT te ponemos en contacto con tus coaches fitness favoritos para que puedas solicitarles dietas y entrenamientos de forma segura, profesional y transparente.
+        Empezar una nueva vida enfocada al deporte y a la alimentación saludable no es fácil, hazlo de la mano de los mejores profesionales.
+      </h6>
+  </div>
+  <br>
+  <div class="jumbotron row">
+    <div class="col-sm-6">
+      <ul>
+        <li><img src="http://localhost/fitit_v2/images/qef.png">Perfil privado</li>
+        <li class="margin_top"><img src="http://localhost/fitit_v2/images/qef.png">Contacto directo con tu coach</li>
+        <li class="margin_top"><img src="http://localhost/fitit_v2/images/qef.png">Seguridad en el pago</li>
+        <li class="margin_top"><img src="http://localhost/fitit_v2/images/qef.png">Tu plan en menos de 48h</li>
+        <li class="margin_top"><img src="http://localhost/fitit_v2/images/qef.png">Garantia <span id="fitit_font" class="fitit_style"><span class="red">FITIT.</span><span class="grey">ES</span></span></li>
+      </ul>
+    </div>
+    <div class="col-sm-6">
+      <a href="/fitit_v2/#contenido"><button type="submit" class="btn bold" style="font-size: 25px;">¡EMPIEZA TU NUEVA VIDA FIT!</button></a>
+    </div>
   </div>
 
-  <br><br><br><br><br>
+  <div class="size">
+      <h3 class="text-center margin_top bold">FASES PARA EMPEZAR TU NUEVA VIDA FIT</h3>
+      <br>
+      <div class="row" style="padding: 0px 0px 0px 40px;">
+        <div class="col-sm-1 number bold">
+          1
+        </div>
+        <div class="col-sm-11">
+          En la página principal encontrarás todos los perfiles de los coaches que trabajan desde nuestra plataforma. Selecciona al coach que más encaje con tus objetivos. Si sigues a algún influencer fitness en tus redes sociales es posible que lo encuentres aquí, si no…¡MÁNDALE UN MENSAJE PARA QUE SE UNA A LA REVOLUCIÓN DE LOS ASESORAMIENTOS FITNESS ONLINE!
+        </div>
+      </div>
+      <br>
+      <div class="row back">
+        <div class="col-sm-11">
+          Una vez hayas seleccionado a tu coach podrás elegir los diferentes packs que ofrece tu coach o PERSONALIZAR tu asesoramiento fitness
+        </div>
+        <div class="col-sm-1 number bold">
+          2
+        </div>
+      </div>
+      <br>
+      <div class="row" style="padding: 0px 0px 0px 40px;">
+        <div class="col-sm-1 number bold">
+          3
+        </div>
+        <div class="col-sm-11">
+          Por último tendrás que rellenar tus datos personales y elegir el método de pago
+        </div>
+      </div>
+      <br>
+      <div class="row back">
+        <div class="col-sm-11">
+          AHORA SI! Puedes empezar tu nueva vida fit de la mano de tu coach fitness favorito
+        </div>
+        <div class="col-sm-1 number bold">
+          4
+        </div>
+      </div>
+
+  </div>
+
+
+  <br><br><br>
 </div>
